2aa1f5cccc0c827acd2e327cfbb53cf755d5358d
Unknown Developer <olegis019@gmail.com>
2025-09-14 18:22:37 +0400

    new concept

---
+++ lua/dreamwork/fabric.lua
@@ -0,0 +1,36 @@
+---@class dreamwork
+local dreamwork = _G.dreamwork
+local std = dreamwork.std
+
+--- [SHARED AND MENU]
+---
+--- Dreamwork Code Fabric
+---
+--- Library for code pre-build and exclusive features.
+---
+---@class dreamwork.fabric
+local fabric = dreamwork.fabric or {}
+dreamwork.fabric = fabric
+
+
+
+--[[
+
+    TODO: Concept
+
+    #if defined( __SERVER )
+
+        server.tickrate.set( 60 )
+
+    #else
+
+        #include( "./client.lua" )
+
+    #endif
+
+    Maybe more in future
+
+]]
+
+
+
