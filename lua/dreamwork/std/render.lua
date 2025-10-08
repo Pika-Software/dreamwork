dbc6cffa1bc5d9c5e8048614648974f5e58725c6
n1clude <56724583+n1clud3@users.noreply.github.com>
2025-10-09 00:08:03 +0200

    Add std.render.stencil

--- lua/dreamwork/std/render.lua
+++ lua/dreamwork/std/render.lua
@@ -57,5 +57,70 @@ if std.CLIENT then
     render.overrideModelMaterial = render.overrideModelMaterial or glua_render.ModelMaterialOverride
     render.overrideBrushMaterial = render.overrideBrushMaterial or glua_render.BrushMaterialOverride
     render.overrideWorldMaterial = render.overrideWorldMaterial or glua_render.WorldMaterialOverride
-end
 
+    do
+        --- [CLIENT]
+        ---
+        --- Stencil system functions
+        ---@class dreamwork.std.render.stencil
+        local stencil = render.stencil or {}
+        render.stencil = stencil
+
+        stencil.OPERATION = {
+            KEEP = STENCILOPERATION_KEEP,
+            ZERO = STENCILOPERATION_ZERO,
+            REPLACE = STENCILOPERATION_REPLACE,
+            INCRSAT = STENCILOPERATION_INCRSAT,
+            DECRSAT = STENCILOPERATION_DECRSAT,
+            INVERT = STENCILOPERATION_INVERT,
+            INCR = STENCILOPERATION_INCR,
+            DECR = STENCILOPERATION_DECR
+        }
+
+        if not stencil.enable then
+            --- [CLIENT]
+            ---
+            --- Enable stencil system for future operations.
+            stencil.enable = function() glua_render.SetStencilEnable(true) end
+        end
+
+        if not stencil.disable then
+            --- [CLIENT]
+            ---
+            --- Disable stencil system for future operations.
+            stencil.disable = function() glua_render.SetStencilEnable(false) end
+        end
+
+        stencil.clear = stencil.clear or glua_render.ClearStencil
+        stencil.setCompareFunction = stencil.setCompareFunction or glua_render.SetStencilCompareFunction
+        stencil.setFailOperation = stencil.setFailOperation or glua_render.SetStencilFailOperation
+        stencil.setZFailOperation = stencil.setZFailOperation or glua_render.SetStencilZFailOperation
+        stencil.setPassOperation = stencil.setPassOperation or glua_render.SetStencilPassOperation
+        stencil.setTestMask = stencil.setTestMask or glua_render.SetStencilTestMask
+        stencil.setWriteMask = stencil.setWriteMask or glua_render.SetStencilWriteMask
+        stencil.setReferenceValue = stencil.setReferenceValue or glua_render.SetStencilReferenceValue
+        stencil.clearBufferObey = stencil.clearBufferObey or glua_render.ClearBuffersObeyStencil
+        stencil.clearBufferRect = stencil.clearBufferRect or glua_render.ClearStencilBufferRectangle
+        stencil.performFullScreenOperation = stencil.performFullScreenOperation or glua_render.PerformFullScreenStencilOperation
+
+        if not stencil.reset then
+            local stencil_clear = stencil.clear
+            local stencil_setWriteMask = stencil.setWriteMask
+            local stencil_setTestMask = stencil.setTestMask
+            local stencil_setPassOp = stencil.setPassOperation
+            local stencil_setZFailOp = stencil.setFailOperation
+
+            local op_keep = stencil.OPERATION.KEEP
+            --- [CLIENT]
+            ---
+            --- Reset the stencil system to safe values.
+            stencil.reset = function()
+                stencil_clear()
+                stencil_setWriteMask(255)
+                stencil_setTestMask(255)
+                stencil_setPassOp(op_keep)
+                stencil_setZFailOp(op_keep)
+            end
+        end
+    end
+end
