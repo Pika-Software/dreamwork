77ae327386a3529e61ac6cba76fce007496ff43e
n1clude <56724583+n1clud3@users.noreply.github.com>
2025-10-09 00:13:59 +0200

    Shorten fog and beam func definitions

--- lua/dreamwork/std/render.lua
+++ lua/dreamwork/std/render.lua
@@ -15,31 +15,37 @@ std.render = render
 -- TODO: add render/draw/surface functions
 
 if std.CLIENT then
-    --- [CLIENT]
-    ---
-    --- Various fog rendering functions.
-    ---@class dreamwork.std.render.fog
-    render.fog = render.fog or {}
-    render.fog.getMode = render.fog.getMode or glua_render.GetFogMode
-    render.fog.setMode = render.fog.setMode or glua_render.FogMode
-    render.fog.getColor = render.fog.getColor or glua_render.GetFogColor
-    render.fog.setColor = render.fog.setColor or glua_render.FogColor
-    render.fog.getDistances = render.fog.getDistances or glua_render.GetFogDistances
-    render.fog.setStartDistance = render.fog.setStartDistance or glua_render.FogStart
-    render.fog.setEndDistance = render.fog.setEndDistance or glua_render.FogEnd
-    render.fog.getMaxDensity = render.fog.getMaxDensity or glua_render.GetFogMaxDensity
-    render.fog.setMaxDensity = render.fog.setMaxDensity or glua_render.FogMaxDensity
-    render.fog.setZ = render.fog.setZ or glua_render.SetFogZ
+    do
+        --- [CLIENT]
+        ---
+        --- Various fog rendering functions.
+        ---@class dreamwork.std.render.fog
+        local fog = render.fog or {}
+        render.fog = fog
+        fog.getMode = fog.getMode or glua_render.GetFogMode
+        fog.setMode = fog.setMode or glua_render.FogMode
+        fog.getColor = fog.getColor or glua_render.GetFogColor
+        fog.setColor = fog.setColor or glua_render.FogColor
+        fog.getDistances = fog.getDistances or glua_render.GetFogDistances
+        fog.setStartDistance = fog.setStartDistance or glua_render.FogStart
+        fog.setEndDistance = fog.setEndDistance or glua_render.FogEnd
+        fog.getMaxDensity = fog.getMaxDensity or glua_render.GetFogMaxDensity
+        fog.setMaxDensity = fog.setMaxDensity or glua_render.FogMaxDensity
+        fog.setZ = fog.setZ or glua_render.SetFogZ
+    end
 
-    --- [CLIENT]
-    ---
-    --- [Beam](https://wiki.facepunch.com/gmod/render_beams) rendering functions.
-    ---@class dreamwork.std.render.beam
-    render.beam = render.beam or {}
-    render.beam.start = render.beam.start or glua_render.StartBeam
-    render.beam.finish = render.beam.finish or glua_render.EndBeam
-    render.beam.addSegment = render.beam.addSegment or glua_render.AddBeam
-    render.beam.draw = render.beam.draw or glua_render.DrawBeam
+    do
+        --- [CLIENT]
+        ---
+        --- [Beam](https://wiki.facepunch.com/gmod/render_beams) rendering functions.
+        ---@class dreamwork.std.render.beam
+        local beam = render.beam or {}
+        render.beam = beam
+        beam.start = beam.start or glua_render.StartBeam
+        beam.finish = beam.finish or glua_render.EndBeam
+        beam.addSegment = beam.addSegment or glua_render.AddBeam
+        beam.draw = beam.draw or glua_render.DrawBeam
+    end
 
     render.setMaterial = render.setMaterial or glua_render.SetMaterial
     if not render.setColorMaterial then
