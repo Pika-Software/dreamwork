f4974e16612370fd76bb5619a821e491c12e11f8
Unknown Developer <olegis019@gmail.com>
2026-06-22 22:26:51 +0400

    refactor: `punycode` fixes & improvements

--- lua/dreamwork/std/codec/punycode.lua
+++ lua/dreamwork/std/codec/punycode.lua
@@ -3,20 +3,25 @@
 ---@class dreamwork.std
 local std = dreamwork.std
 
+local len = std.len
+
 local string = std.string
 local string_len = string.len
+local string_sub = string.sub
 local string_byte = string.byte
 local string_char = string.char
 
-local math_floor = std.math.floor
+local math = std.math
+local math_max = math.max
+local math_floor = math.floor
 
 local table = std.table
 local table_concat = table.concat
 local table_insert = table.insert
 
 local utf8 = std.utf8
-local utf8_unpack = utf8.unpack
 local utf8_pack = utf8.pack
+local utf8_unpack = utf8.unpack
 
 --- [SHARED AND MENU]
 ---
@@ -27,10 +32,8 @@ local utf8_pack = utf8.pack
 --- RFC 3492: https://datatracker.ietf.org/doc/html/rfc3492
 ---
 ---@class dreamwork.std.punycode
-local punycode = std.punycode or {}
+local punycode = {}
 std.punycode = punycode
-
--- TODO: rewrite with by byte analysis
 
 --- [SHARED AND MENU]
 ---
@@ -39,12 +42,12 @@ std.punycode = punycode
 ---@param codepoints dreamwork.std.utf8.Sequence The code points to encode.
 ---@param codepoint_count? integer The number of code points to encode.
 ---@param ignore_prefix? boolean If `true`, the prefix `"xn--"` will be ignored.
----@param lax? boolean `true` if lax mode should be used in UTF-8 encoding/decoding.
+---@param lax? boolean Whether to lax the UTF-8 validity check.
 ---@return string | nil punycode_str The Punycode string, `nil` if an error occurred.
 ---@return nil | string err_msg The error message, `nil` if no error occurred.
-local function pack( codepoints, codepoint_count, ignore_prefix, lax )
+local function punycode_pack( codepoints, codepoint_count, ignore_prefix, lax )
     if codepoint_count == nil then
-        codepoint_count = #codepoints
+        codepoint_count = len( codepoints )
     end
 
     local segments, segment_count = {}, 0
@@ -111,21 +114,19 @@ local function pack( codepoints, codepoint_count, ignore_prefix, lax )
                 local q = delta
                 local k = 0x24
 
-                while true do
-                    local t
+                ::punycode_encode_loop::
 
-                    if k <= bias then
-                        t = 0x1
-                    elseif k >= (bias + 0x1A) then
-                        t = 0x1A
-                    else
-                        t = k - bias
-                    end
+                local t
 
-                    if q < t then
-                        break
-                    end
+                if k <= bias then
+                    t = 0x1
+                elseif k >= (bias + 0x1A) then
+                    t = 0x1A
+                else
+                    t = k - bias
+                end
 
+                if q >= t then
                     local q_minus_t = q - t
                     local base_minus_t = 0x24 - t
 
@@ -142,6 +143,8 @@ local function pack( codepoints, codepoint_count, ignore_prefix, lax )
 
                     q = math_floor( q_minus_t / base_minus_t )
                     k = k + 0x24
+
+                    goto punycode_encode_loop
                 end
 
                 local digit = q
@@ -182,172 +185,175 @@ local function pack( codepoints, codepoint_count, ignore_prefix, lax )
     return table_concat( segments, "", 1, segment_count )
 end
 
-punycode.pack = pack
+punycode.pack = punycode_pack
 
-local unpack
-do
-
-    local string_sub = string.sub
-
-    --- [SHARED AND MENU]
-    ---
-    --- Unpacks a Punycode string.
-    ---
-    ---@param punycode_str string The Punycode string to unpack.
-    ---@param ignore_prefix? boolean If `true`, the prefix `"xn--"` will be ignored.
-    ---@param lax? boolean `true` if lax mode should be used in UTF-8 encoding/decoding.
-    ---@return dreamwork.std.utf8.Sequence | nil codepoints The codepoints of the unpacked string, `nil` if an error occurred.
-    ---@return integer | nil codepoint_count The number of codepoints in the string, `nil` if an error occurred.
-    ---@return nil | string err_msg The error message, `nil` if no error occurred.
-    function unpack( punycode_str, ignore_prefix, lax )
-        if not ignore_prefix then
-            local uint8_1, uint8_2, uint8_3, uint8_4 = string_byte( punycode_str, 1, 4 )
-            if uint8_1 == nil then
-                return nil, nil, "punycode string cannot be empty"
-            end
-
-            if uint8_1 == 0x78 --[[ "x" ]] and uint8_2 == 0x6E --[[ "n" ]] and uint8_3 == 0x2D --[[ "-" ]] and uint8_4 == 0x2D --[[ "-" ]] then
-                punycode_str = string_sub( punycode_str, 5 )
-            else
-                return utf8_unpack( punycode_str, 1, string_len( punycode_str ), lax )
-            end
+--- [SHARED AND MENU]
+---
+--- Unpacks a Punycode string.
+---
+---@param punycode_str string The Punycode string to unpack.
+---@param ignore_prefix? boolean If `true`, the prefix `"xn--"` will be ignored.
+---@param lax? boolean Whether to lax the UTF-8 validity check.
+---@return dreamwork.std.utf8.Sequence | nil codepoints The codepoints of the unpacked string, `nil` if an error occurred.
+---@return integer | nil codepoint_count The number of codepoints in the string, `nil` if an error occurred.
+---@return nil | string err_msg The error message, `nil` if no error occurred.
+local function punycode_unpack( punycode_str, ignore_prefix, lax )
+    if not ignore_prefix then
+        local uint8_1, uint8_2, uint8_3, uint8_4 = string_byte( punycode_str, 1, 4 )
+        if uint8_1 == nil then
+            return nil, nil, "punycode string cannot be empty"
         end
 
-        local punycode_str_length = string_len( punycode_str )
-
-        local basic = 0
-
-        for j = 1, punycode_str_length do
-            if string_byte( punycode_str, j, j ) == 0x2D --[[ "-" ]] then
-                basic = j - 1
-            end
+        if uint8_1 == 0x78 --[[ "x" ]] and uint8_2 == 0x6E --[[ "n" ]] and uint8_3 == 0x2D --[[ "-" ]] and uint8_4 == 0x2D --[[ "-" ]] then
+            punycode_str = string_sub( punycode_str, 5 )
+        else
+            return utf8_unpack( punycode_str, 1, string_len( punycode_str ), lax )
         end
+    end
 
-        local codepoints, codepoint_count = {}, 0
+    local punycode_str_length = string_len( punycode_str )
 
+    local basic = 0
+
+    for j = 1, punycode_str_length do
+        if string_byte( punycode_str, j, j ) == 0x2D --[[ "-" ]] then
+            basic = j - 1
+        end
+    end
+
+    local codepoint_count = basic
+    local codepoints
+
+    if basic > 8000 then
+        codepoints = { string_byte( punycode_str, 1, 8000 ) }
+
+        for j = 8001, basic, 1 do
+            codepoints[ j ] = string_byte( punycode_str, j, j )
+        end
+    else
+        codepoints = { string_byte( punycode_str, 1, basic ) }
+    end
+
+    if not lax then
         for j = 1, basic, 1 do
-            local uint8 = string_byte( punycode_str, j, j )
-            if uint8 < 0x80 then
-                codepoint_count = codepoint_count + 1
-                codepoints[ codepoint_count ] = uint8
-            else
+            if codepoints[ j ] >= 0x80 then
                 return nil, nil, "not basic exception occurred"
             end
         end
+    end
 
-        local index
+    local index
 
-        if basic == 0 then
-            index = 0
-        else
-            index = basic + 1
+    if basic == 0 then
+        index = 0
+    else
+        index = basic + 1
+    end
+
+    local unicode_init = 0x80
+    local delta_init = 0
+    local bias = 0x48
+
+    while index < punycode_str_length do
+        local oldi = delta_init
+        local w = 1
+        local k = 0x24
+
+        ::punycode_decode_loop::
+
+        if index >= punycode_str_length then
+            return nil, nil, "invalid input exception occurred"
         end
 
-        local unicode_init = 0x80
-        local delta_init = 0
-        local bias = 0x48
+        index = index + 1
 
-        while index < punycode_str_length do
-            local oldi = delta_init
-            local w = 1
-            local k = 0x24
+        local digit = string_byte( punycode_str, index, index )
 
-            while true do
-                if index >= punycode_str_length then
-                    return nil, nil, "invalid input exception occurred"
-                end
+        if digit < 0x3A then
+            digit = digit - 0x16
+        elseif digit < 0x5B then
+            digit = digit - 0x41
+        elseif digit < 0x7B then
+            digit = digit - 0x61
+        else
+            digit = 0x24
+        end
 
-                index = index + 1
+        if digit >= 0x24 or digit > math_floor( (0x7FFFFFFF - delta_init) / w ) then
+            return nil, nil, "overflow exception occurred"
+        end
 
-                local digit = string_byte( punycode_str, index, index )
+        delta_init = delta_init + digit * w
 
-                if digit < 0x3A then
-                    digit = digit - 0x16
-                elseif digit < 0x5B then
-                    digit = digit - 0x41
-                elseif digit < 0x7B then
-                    digit = digit - 0x61
-                else
-                    digit = 0x24
-                end
+        local t
 
-                if digit >= 0x24 or digit > math_floor( (0x7FFFFFFF - delta_init) / w ) then
-                    return nil, nil, "overflow exception occurred"
-                end
-
-                delta_init = delta_init + digit * w
-
-                local t
-                if k > bias then
-                    if k < (bias + 0x1A) then
-                        t = k - bias
-                    else
-                        t = 0x1A
-                    end
-                else
-                    t = 0x1
-                end
-
-                if digit < t then
-                    break
-                end
-
-                local base_minus_t = 0x24 - t
-
-                if w > math_floor( 0x7FFFFFFF / base_minus_t ) then
-                    return nil, nil, "overflow exception occurred"
-                end
-
-                w = w * base_minus_t
-                k = k + 0x24
-            end
-
-            codepoint_count = #codepoints
-
-            local out = codepoint_count + 1
-            local delta = delta_init - oldi
-
-            if oldi == 0 then
-                delta = math_floor( delta / 0x2BC )
+        if k > bias then
+            if k < (bias + 0x1A) then
+                t = k - bias
             else
-                delta = math_floor( delta * 0.5 )
+                t = 0x1A
             end
+        else
+            t = 0x1
+        end
 
-            delta = delta + math_floor( delta / out )
-            k = 0
+        if digit >= t then
+            local base_minus_t = 0x24 - t
 
-            while delta > 0x1C7 do
-                delta = math_floor( delta / 0x23 )
-                k = k + 0x24
-            end
-
-            bias = k + math_floor( 0x24 * delta / (delta + 0x26) )
-
-            -- `delta_init` was supposed to wrap around from `out` to `0`,
-            -- incrementing `unicode_init` each time, so we'll fix that now:
-            local i_div_out = math_floor( delta_init / out )
-
-            if i_div_out > (0x7FFFFFFF - unicode_init) then
+            if w > math_floor( 0x7FFFFFFF / base_minus_t ) then
                 return nil, nil, "overflow exception occurred"
             end
 
-            unicode_init = unicode_init + i_div_out
-            delta_init = delta_init % out
+            w = w * base_minus_t
+            k = k + 0x24
 
-            for temp = codepoint_count, delta_init, -1 do
-                codepoints[ temp + 1 ] = codepoints[ temp ]
-            end
-
-            delta_init = delta_init + 1
-            codepoints[ delta_init ] = unicode_init
+            goto punycode_decode_loop
         end
 
-        return codepoints, #codepoints
+        codepoint_count = #codepoints
+
+        local out = codepoint_count + 1
+        local delta = delta_init - oldi
+
+        if oldi == 0 then
+            delta = math_floor( delta / 0x2BC )
+        else
+            delta = math_floor( delta * 0.5 )
+        end
+
+        delta = delta + math_floor( delta / out )
+        k = 0
+
+        while delta > 0x1C7 do
+            delta = math_floor( delta / 0x23 )
+            k = k + 0x24
+        end
+
+        bias = k + math_floor( 0x24 * delta / (delta + 0x26) )
+
+        -- `delta_init` was supposed to wrap around from `out` to `0`,
+        -- incrementing `unicode_init` each time, so we'll fix that now:
+        local i_div_out = math_floor( delta_init / out )
+
+        if i_div_out > (0x7FFFFFFF - unicode_init) then
+            return nil, nil, "overflow exception occurred"
+        end
+
+        unicode_init = unicode_init + i_div_out
+        delta_init = delta_init % out
+
+        for temp = codepoint_count, math_max( 1, delta_init ), -1 do
+            codepoints[ temp + 1 ] = codepoints[ temp ]
+        end
+
+        delta_init = delta_init + 1
+        codepoints[ delta_init ] = unicode_init
     end
 
+    return codepoints, #codepoints -- TODO: replace with dynamic re-calc instead of #tbl
 end
 
-punycode.unpack = unpack
+punycode.unpack = punycode_unpack
 
 --- [SHARED AND MENU]
 ---
@@ -357,12 +363,33 @@ punycode.unpack = unpack
 ---@param ignore_prefix? boolean If `true`, the prefix `"xn--"` will be ignored.
 ---@param start_position? integer The position to start from in bytes.
 ---@param end_position? integer The position to end at in bytes.
----@param lax? boolean `true` if lax mode should be used in UTF-8 encoding/decoding.
+---@param lax? boolean Whether to lax the UTF-8 validity check.
 ---@return string | nil punycode_str The Punycode string, `nil` if an error occurred.
 ---@return nil | string err_msg The error message, `nil` if no error occurred.
 function punycode.encode( str, ignore_prefix, start_position, end_position, lax )
-    local codepoints, codepoint_count = utf8_unpack( str, start_position, end_position, lax )
-    return pack( codepoints, codepoint_count, ignore_prefix, lax )
+    local labels, label_count = string.byteSplit( str, 0x2E --[[ . ]], start_position, end_position )
+    if label_count == 0 then
+        return "", nil
+    end
+
+    for i = 1, label_count, 1 do
+        local codepoints, codepoint_count = utf8_unpack( labels[ i ], nil, nil, lax )
+
+        local punycode_str, err_msg = punycode_pack( codepoints, codepoint_count, ignore_prefix, lax )
+        if punycode_str == nil then
+            return nil, err_msg
+        end
+
+        labels[ i ] = punycode_str
+    end
+
+    if label_count == 1 then
+        return labels[ 1 ], nil
+    elseif label_count == 2 then
+        return labels[ 1 ] .. "." .. labels[ 2 ], nil
+    else
+        return table_concat( labels, ".", 1, label_count ), nil
+    end
 end
 
 --- [SHARED AND MENU]
@@ -371,14 +398,31 @@ end
 ---
 ---@param punycode_str string The Punycode string to decode.
 ---@param ignore_prefix? boolean If `true`, the prefix `"xn--"` will be ignored.
----@param lax? boolean `true` if lax mode should be used in UTF-8 encoding/decoding.
+---@param start_position? integer The position to start from in bytes.
+---@param end_position? integer The position to end at in bytes.
+---@param lax? boolean Whether to lax the UTF-8 validity check.
 ---@return string | nil str The decoded string, `nil` if an error occurred.
 ---@return nil | string err_msg The error message, `nil` if no error occurred.
-function punycode.decode( punycode_str, ignore_prefix, lax )
-    local codepoints, codepoint_count, err_msg = unpack( punycode_str, ignore_prefix, lax )
-    if codepoints == nil then
-        return nil, err_msg
+function punycode.decode( punycode_str, ignore_prefix, start_position, end_position, lax )
+    local labels, label_count = string.byteSplit( punycode_str, 0x2E --[[ . ]], start_position, end_position )
+    if label_count == 0 then
+        return "", nil
+    end
+
+    for i = 1, label_count, 1 do
+        local codepoints, codepoint_count, err_msg = punycode_unpack( labels[ i ], ignore_prefix, lax )
+        if codepoints == nil then
+            return nil, err_msg
+        end
+
+        labels[ i ] = utf8_pack( codepoints, codepoint_count, lax )
+    end
+
+    if label_count == 1 then
+        return labels[ 1 ], nil
+    elseif label_count == 2 then
+        return labels[ 1 ] .. "." .. labels[ 2 ], nil
     else
-        return utf8_pack( codepoints, codepoint_count, lax )
+        return table_concat( labels, ".", 1, label_count ), nil
     end
 end
