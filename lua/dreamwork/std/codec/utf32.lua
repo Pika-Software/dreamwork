42150835999743bc006904de848953731a637362
Unknown Developer <olegis019@gmail.com>
2026-07-22 17:07:54 +0400

    utf32 todos

--- lua/dreamwork/std/codec/utf32.lua
+++ lua/dreamwork/std/codec/utf32.lua
@@ -2,5 +2,26 @@
 local std = dreamwork.std
 
 ---@class dreamwork.std.encoding.utf32
-local utf32 = std.utf32 or {}
+local utf32 = {}
 std.utf32 = utf32
+
+---@alias dreamwork.std.utf32.Codepoint integer
+
+
+
+-- len
+
+-- pack
+-- unpack
+-- normalize
+
+-- reverse
+-- sub
+
+-- char
+-- codepoint
+-- offset
+-- codes
+
+-- lower
+-- upper
