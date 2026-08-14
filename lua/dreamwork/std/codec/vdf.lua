b35978fd467e276888e760f1f536ee64cf66e4db
Unknown Developer <olegis019@gmail.com>
2026-08-14 16:37:58 +0400

    refactor: `vdf` changes & todo

--- lua/dreamwork/std/codec/vdf.lua
+++ lua/dreamwork/std/codec/vdf.lua
@@ -1,3 +1,8 @@
+---@class dreamwork.GModUtilLibrary
+---@field TableToKeyValues fun( tbl: table, rootKey: string? ): string
+---@field KeyValuesToTable fun( keyValues: string, usesEscapeSequences: boolean?, preserveKeyCase: boolean? ): table
+---@field KeyValuesToTablePreserveOrder fun( keyValues: string, usesEscapeSequences: boolean?, preserveKeyCase: boolean? ): table
+---@diagnostic disable-next-line: undefined-global
 local glua_util = util
 
 ---@class dreamwork.std
@@ -8,8 +13,10 @@ local std = dreamwork.std
 --- The KeyValues format is used in the Source engine to store meta data for resources, scripts, materials, VGUI elements, and more..
 ---
 ---@class dreamwork.std.vdf
-local vdf = std.vdf or {}
+local vdf = {}
 std.vdf = vdf
 
-vdf.serialize = vdf.serialize or glua_util.TableToKeyValues
-vdf.deserialize = vdf.deserialize or glua_util.KeyValuesToTable
+-- TODO: fallback to custom implementation
+
+vdf.serialize = glua_util.TableToKeyValues
+vdf.deserialize = glua_util.KeyValuesToTablePreserveOrder
